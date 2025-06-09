efd8f4e4f23067786bb756923b3119163166a891
Agvensome <651752508@qq.com>
2025-06-09 20:50:43 +0800

    update petsc

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <petsc.h>
+
+int main(int argc, char **argv) {
+    PetscErrorCode ierr;
+    PetscInt i, j, nS = 100, nT = 1000;
+    PetscReal Smax = 100.0, T = 1.0, K = 50.0, r = 0.05, sigma = 0.2;
+    \
+	
+	
+
+
+	return 0; 
+} 
