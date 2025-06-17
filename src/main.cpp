286d07f42e64f1b9c8dc1ebdc90f2737ab10fbe3
Agvensome <651752508@qq.com>
2025-06-17 22:44:44 +0800

    boundary application

--- src/main.cpp
+++ src/main.cpp
@@ -55,3 +55,41 @@ void set_initial_condition(double *u, int local_size, int offset, OptionParams *
         }
     }
 }
+
+// 应用边界条件
+void apply_boundary_condition(double *u, int local_size, int rank, int size, 
+                             double t, OptionParams *params) {
+    // 左边界 (rank 0的第一个点)
+    if (rank == 0) {
+        u[0] = left_boundary_value(t, params);
+    }
+    
+    // 右边界 (最后一个rank的最后一个点)
+    if (rank == size - 1) {
+        u[local_size - 1] = right_boundary_value(t, params);
+    }
+}
+
+// 交换边界数据
+void exchange_boundary_data(double *u, int local_size, int rank, int size, MPI_Comm comm) {
+    double left_bound = 0.0, right_bound = 0.0;
+    MPI_Status status;
+    
+    // 发送右边界给下一个进程，接收左边界从上一个进程
+    if (rank < size - 1) {
+        MPI_Send(&u[local_size - 1], 1, MPI_DOUBLE, rank + 1, 0, comm);
+    }
+    if (rank > 0) {
+        MPI_Recv(&left_bound, 1, MPI_DOUBLE, rank - 1, 0, comm, &status);
+        u[0] = left_bound;
+    }
+    
+    // 发送左边界给上一个进程，接收右边界从下一个进程
+    if (rank > 0) {
+        MPI_Send(&u[0], 1, MPI_DOUBLE, rank - 1, 1, comm);
+    }
+    if (rank < size - 1) {
+        MPI_Recv(&right_bound, 1, MPI_DOUBLE, rank + 1, 1, comm, &status);
+        u[local_size - 1] = right_bound;
+    }
+}
